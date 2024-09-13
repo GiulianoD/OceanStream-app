8293cf030796adaeab4703c60e84919a329c56ba
GiulianoD <giuliano.demoner@gmail.com>
2024-09-13 10:13:34 -0300

    Finalizado parte lógica login

--- main.py
+++ main.py
@@ -1,61 +1,130 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-
 import requests
-API_agify = "https://api.agify.io?name=NOME"
+import json
+from plyer import storagepath
+import os
+import jwt
+from datetime import datetime
 
-GUI = Builder.load_file("tela.kv")
+JWT_FILE = "oceanstream.jwt"
 
-class TelaLogin(GridLayout):
-    def __init__(self, **kwargs):
-        super(TelaLogin, self).__init__(**kwargs)
-        self.cols = 1
+class Overview(Screen):
+    pass
 
-        self.top_grid = GridLayout()
-        self.top_grid.cols = 2
+# class TelaLogin2(Screen):
+#     pass
 
-        self.top_grid.add_widget(Label(text="E-Mail: "))
-        self.email = TextInput(multiline=False)
-        self.top_grid.add_widget(self.email)
+class WindowManager(ScreenManager):
+    pass
 
-        self.top_grid.add_widget(Label(text="Senha: "))
-        self.senha = TextInput(multiline=False)
-        self.top_grid.add_widget(self.senha)
+class TelaLogin(Screen):
+    email = ObjectProperty(None)
+    senha = ObjectProperty(None)
 
-        self.add_widget(self.top_grid)
+    def on_enter(self):
+        token = self.get_access_token()
+        if token:
+            if self.is_token_valid(token):
+                print("Token válido, redirecionando...")
+                # self.manager.current = 'overview'
+            else:
+                print("Token expirado, deletando...")
+                self.delete_access_token()
 
-        self.submit = Button(text="Login", font_size=32)
-        self.submit.bind(on_press=self.press)
-        self.add_widget(self.submit)
+    def submit(self):
+        email = self.ids.email.text
+        senha = self.ids.senha.text
 
-    def press(self, instance):
-        email = self.email.text
-        senha = self.senha.text
+        # Montar o payload JSON
+        payload = {
+            "email": email,
+            "senha": senha
+        }
 
-        print(f'Email: {email} | Senha: {senha}')
-        self.add_widget(Label(text=f'Email: {email} | Senha: {senha}'))
+        # Fazer a requisição POST
+        url = 'https://oceanstream-8b3329b99e40.herokuapp.com/login'
+        headers = {'Content-Type': 'application/json'}
+        
+        try:
+            response = requests.post(url, data=json.dumps(payload), headers=headers)
+            
+            if response.status_code == 200:
+                # Obter a resposta
+                data = response.json()
+                access_token = data.get('accessToken')
+
+                # Armazenar o accessToken de forma persistente
+                self.store_access_token(access_token)
+                self.manager.current = 'overview'
+            else:
+                print(f"Falha no login: {response.status_code} - {response.text}")
+        
+        except Exception as e:
+            print(f"Erro ao tentar fazer login: {str(e)}")
+        
+        # Limpar o campo de senha
+        self.ids.senha.text = ""
+
+    def store_access_token(self, token):
+        # Salvar o token em um arquivo persistente
+        app_storage_dir = storagepath.get_home_dir()  # Diretório de armazenamento do app
+        token_file_path = os.path.join(app_storage_dir, JWT_FILE)
+
+        with open(token_file_path, 'w') as token_file:
+            token_file.write(token)
+        print(f"Token salvo em {token_file_path}")
+
+    def get_access_token(self):
+        # Recuperar o token armazenado
+        app_storage_dir = storagepath.get_home_dir()
+        token_file_path = os.path.join(app_storage_dir, JWT_FILE)
+
+        if os.path.exists(token_file_path):
+            with open(token_file_path, 'r') as token_file:
+                token = token_file.read()
+                print(f"Token recuperado: {token}")
+                return token
+        else:
+            print("Nenhum token encontrado.")
+            return None
+
+    def delete_access_token(self):
+        # Deletar o token armazenado
+        app_storage_dir = storagepath.get_home_dir()
+        token_file_path = os.path.join(app_storage_dir, JWT_FILE)
+
+        if os.path.exists(token_file_path):
+            os.remove(token_file_path)
+            print("Token deletado.")
+        else:
+            print("Nenhum token para deletar.")
+
+    def is_token_valid(self, token):
+        try:
+            # Decodificar o token JWT para verificar a validade
+            decoded_token = jwt.decode(token, options={"verify_signature": False})
+            exp_timestamp = decoded_token.get('exp')
+            if exp_timestamp:
+                exp_date = datetime.fromtimestamp(exp_timestamp)
+                if exp_date > datetime.now():
+                    return True
+                else:
+                    return False
+            return False
+        except Exception as e:
+            print(f"Erro ao verificar token: {str(e)}")
+            return False
+
+Builder.load_file('paginas/login.kv')
+Builder.load_file('paginas/overview.kv')
+kv = Builder.load_file('paginas/windowmanager.kv')
 
 class OceanStream(App):
     def build(self):
-        return TelaLogin()
-        # return GUI
+        return kv
 
-    # def on_start(self):
-    #     self.root.ids["titulo"].text = f"{self.agify('Giuliano')}"
-
-    def agify(selft, nome):
-        link = API_agify.replace('NOME', nome)
-        requisicao = requests.get(link)
-
-        if requisicao.status_code is not 200:
-            return None
-
-        dic_requisicao = requisicao.json()
-        idade = dic_requisicao["age"]
-        return idade
-
-OceanStream().run()
+if __name__ == '__main__':
+    OceanStream().run()
