7a256b4b8b3f98795a50e8636362a162dd8b8726
GiulianoD <giuliano.demoner@gmail.com>
2024-12-09 15:17:10 -0300

    Overview - Estrutura base

--- navigation_bar.py
+++ navigation_bar.py
@@ -5,6 +5,7 @@ from kivy.uix.label import Label
 from kivy.animation import Animation
 from kivy.metrics import dp
 from kivy.graphics import Color, RoundedRectangle
+from kivy.graphics import Ellipse
 
 
 class NavigationBar(MDBoxLayout):
@@ -33,31 +34,39 @@ class NavigationBar(MDBoxLayout):
             )
             self.toolbar.bind(size=self.update_bg, pos=self.update_bg)
 
+        # Adicionar círculo branco abaixo da logo
+        with self.toolbar.canvas:
+            Color(1, 1, 1, 1)  # Cor branca
+            self.circle = Ellipse(
+                size=(dp(100), dp(100)),  # Tamanho do círculo
+                pos=(self.toolbar.x + (self.toolbar.width - dp(100)) / 2, self.toolbar.top - dp(50))  # Posição ajustada
+            )
+
         # Substitui o botão de expansão por um botão com a logo do app
         self.expand_button = MDIconButton(
-            icon="logo.png",  # Caminho para a logo
+            icon="res/logo.png",
             size_hint=(None, None),
             size=(dp(80), dp(80)),
-            icon_size="80sp",  # Ajusta o tamanho do ícone dentro do botão
+            icon_size="80sp",
             theme_text_color="Custom",
-            text_color=(0.02, 0.58, 0.61, 1),  # Cor #04949C
+            text_color=(0.02, 0.58, 0.61, 1), # hex 04949C
             pos_hint={'center_x': 0.5},
             on_release=self.toggle_toolbar
         )
 
         # Título "Menu", com nova cor, fonte em negrito e fonte maior
-        self.menu_title = Label(
-            text="Menu",
-            font_size="28sp",  # Aumenta o tamanho do texto
-            halign="center",
-            valign="middle",  # Centraliza verticalmente
-            size_hint=(None, None),  # Remova dependências de tamanho automático
-            size=(dp(120), dp(40)),  # Define tamanho fixo para garantir a exibição
-            opacity=0,  # Começa invisível
-            pos_hint={'center_x': 0.5},  # Centraliza horizontalmente
-            color=(0.2, 0.6, 0.8, 1),  # Cor #3399CC
-            bold=True  # Adiciona negrito à fonte
-        )
+        # self.menu_title = Label(
+        #     text="Menu",
+        #     font_size="28sp",
+        #     halign="center",
+        #     valign="middle",
+        #     size_hint=(None, None),  # Remova dependências de tamanho automático
+        #     size=(dp(120), dp(40)),  # Define tamanho fixo para garantir a exibição
+        #     opacity=0,  # Começa invisível
+        #     pos_hint={'center_x': 0.5},
+        #     color=(0.2, 0.6, 0.8, 1), # hex 3399CC
+        #     bold=True
+        # )
 
         # Caixa expansível para as opções, inicialmente oculta
         self.options_box = BoxLayout(
@@ -78,7 +87,7 @@ class NavigationBar(MDBoxLayout):
         options = [
             {"text": "Configuração", "icon": "cog", "screen": "configuracao"},
             {"text": "Alertas", "icon": "bell", "screen": "alertas"},
-            {"text": "Informações", "icon": "information", "screen": "informacoes"},
+            {"text": "Overview", "icon": "information", "screen": "overview"},
             {"text": "Suporte", "icon": "help-circle", "screen": "suporte"},
             {"text": "Ocorrência", "icon": "alert", "screen": "ocorrencia"},
         ]
@@ -134,7 +143,7 @@ class NavigationBar(MDBoxLayout):
 
         # Adicionar componentes à toolbar em ordem
         self.toolbar.add_widget(self.expand_button)  # Botão com a logo
-        self.toolbar.add_widget(self.menu_title)      # Título "Menu" abaixo do ícone de expansão
+        # self.toolbar.add_widget(self.menu_title)      # Título "Menu" abaixo do ícone de expansão
         self.toolbar.add_widget(self.options_box)     # Caixa de opções inicialmente vazia
         self.add_widget(self.screen_manager)
         self.add_widget(self.toolbar)
@@ -148,7 +157,7 @@ class NavigationBar(MDBoxLayout):
             anim_options = Animation(height=dp(150), d=0.3)  # Expande a altura das opções
             anim_toolbar = Animation(height=dp(206), d=0.3)  # Expande a altura total da toolbar
             anim_title = Animation(opacity=1, d=0.3)         # Exibe o título "Menu"
-            anim_title.start(self.menu_title)
+            
         else:
             # Recolhe a área de opções e oculta o título "Menu"
             anim_options = Animation(height=0, d=0.3)
@@ -157,15 +166,16 @@ class NavigationBar(MDBoxLayout):
 
             # Remove os widgets para que não possam ser clicados
             self.options_box.clear_widgets()
-            anim_title.start(self.menu_title)
 
         # Inicia animações
         anim_toolbar.start(self.toolbar)
         anim_options.start(self.options_box)
+        # anim_title.start(self.menu_title)
 
     def update_bg(self, *args):
         self.bg_rect.size = self.toolbar.size
         self.bg_rect.pos = self.toolbar.pos
+        self.circle.pos = (self.toolbar.center_x - dp(50), self.toolbar.height - dp(50))
 
     def switch_to_screen(self, screen_name):
         # Mudar para a tela especificada
